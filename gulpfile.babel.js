c11a640fa58a7647923e5d470afddbdc459dcf04
1ambda <1amb4a@gmail.com>
2015-12-27 22:49:33 +0900

    style(build): Resolve lint error on gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,21 +1,21 @@
 'use strict';
 
-import env   from './env.json'
+import env   from "./env.json";
 
-import ts    from 'gulp-typescript'
-import gulp  from 'gulp'
-import path  from 'path'
-import babel from 'gulp-babel'
-import watch from 'gulp-watch'
-import clean from 'gulp-clean'
-import merge from 'merge2';
+import ts    from "gulp-typescript";
+import gulp  from "gulp";
+import path  from "path";
+import babel from "gulp-babel";
+import watch from "gulp-watch";
+import clean from "gulp-clean";
+import merge from "merge2";
 
 import tslint         from "gulp-tslint";
-import jasmine        from 'gulp-jasmine'
-import reporters      from 'jasmine-reporters'
-import sourcemaps     from 'gulp-sourcemaps'
-import runSequence    from 'run-sequence'
-import jasmineBrowser from 'gulp-jasmine-browser'
+import jasmine        from "gulp-jasmine";
+import reporters      from "jasmine-reporters";
+import sourcemaps     from "gulp-sourcemaps";
+import runSequence    from "run-sequence";
+import jasmineBrowser from "gulp-jasmine-browser";
 
 const WATCH_TARGET = [env.FILE.SOURCE_TS, env.FILE.TEST_TS, env.FILE.IGNORED_ALL_D_TS];
 const CLEAN_TARGET = [
@@ -28,27 +28,27 @@ const CLEAN_TARGET = [
   `${env.DIR.BASE_TEST}/${env.FILE.ALL_D_TS}`
 ];
 
-gulp.task('build', () => {
-  runSequence('lint', 'clean', 'compile-src', 'compile-test', 'test-console');
+gulp.task("build", () => {
+  runSequence("lint", "clean", "compile-src", "compile-test", "test-console");
 });
 
-gulp.task('clean', () => {
+gulp.task("clean", () => {
   return gulp.src(CLEAN_TARGET, {read: false})
     .pipe(clean());
 });
 
-gulp.task('con-test', () => {
+gulp.task("con-test", () => {
   gulp.watch(WATCH_TARGET, () => {
     runSequence(
-      'clean',
-      'compile',
-      'test-console');
+      "clean",
+      "compile",
+      "test-console");
   });
 });
 
-gulp.task('con-compile', ()=> {
+gulp.task("con-compile", ()=> {
   gulp.watch(WATCH_TARGET, () => {
-    runSequence('clean', 'compile');
+    runSequence("clean", "compile");
   });
 });
 
@@ -58,7 +58,7 @@ gulp.task("tslint", () => {
     .pipe(tslint.report("full"));
 });
 
-gulp.task('test-console', () => {
+gulp.task("test-console", () => {
   /** pre-task: assert env vars */
   env.ASSERTED_ENV.forEach(envVar => {
     assertEnv(envVar);
@@ -84,15 +84,15 @@ gulp.task('test-console', () => {
     }));
 });
 
-gulp.task('test-browser', () => {
+gulp.task("test-browser", () => {
   return gulp.src([env.FILE.TEST_JS])
     .pipe(watch(WATCH_TARGET))
     .pipe(jasmineBrowser.specRunner())
     .pipe(jasmineBrowser.server({port: 8888}));
 });
 
-gulp.task('compile', () => {
-  const tsProject = ts.createProject('tsconfig.json', {
+gulp.task("compile", () => {
+  const tsProject = ts.createProject("tsconfig.json", {
     declaration: true
   });
 
@@ -108,8 +108,8 @@ gulp.task('compile', () => {
     tsResult /** create `js`, `js.map` */
       .js
       .pipe(sourcemaps.init())
-      .pipe(babel({ presets: ['es2015'] }))
-      .pipe(sourcemaps.write('.'))
+      .pipe(babel({ presets: ["es2015"] }))
+      .pipe(sourcemaps.write("."))
       .pipe(gulp.dest(env.DIR.BUILD))
   ])
 });
@@ -118,7 +118,7 @@ function assertEnv(envVar) {
   const envValue = process.env[envVar];
   const EMPTY_STRING = "";
 
-  if (typeof envValue === 'undefined' || EMPTY_STRING === envValue)
+  if (typeof envValue === "undefined" || EMPTY_STRING === envValue)
     throw new Error(`Invalid ENV Variable: ${envVar}`)
 }
 
