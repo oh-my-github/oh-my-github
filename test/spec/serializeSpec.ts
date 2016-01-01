4f5c6b55df5316ef8bcefdbbaeaa4c8fe25527de
1ambda <1amb4a@gmail.com>
2016-01-01 16:52:58 +0900

    refactor(resource): Add sampleResource.ts

--- test/spec/serializeSpec.ts
+++ test/spec/serializeSpec.ts
@@ -4,12 +4,12 @@
 
 import {deserialize, Deserializable} from "../../src/serialize";
 import {Option, Command} from "../../src/command";
+import {SampleResources} from "./sampleResource"
 
 class Car extends Deserializable {
   @deserialize public engine: string;
   @deserialize public wheels: number;
 }
-
 
 describe("serialize", () => {
   describe("deserialize", () => {
@@ -22,75 +22,7 @@ describe("serialize", () => {
     });
 
     it("should parse Command", () => {
-
-      let raw = { commands:
-        [ { commands: new Array<Command>(),
-          options: new Array<Option>(),
-          _execs: {},
-          _allowUnknownOption: false,
-          _args: new Array<string>(),
-          _name: 'profile',
-          _noHelp: false,
-          parent: new Array<any>(),
-          _description: 'get github profile using the provided token',
-          _events: {},
-          _eventsCount: 2 },
-          { commands: new Array<Command>(),
-            options: new Array<Option>(),
-            _execs: {},
-            _allowUnknownOption: false,
-            _args: new Array<string>(),
-            _name: 'exec',
-            _noHelp: false,
-            parent: new Array<any>(),
-            _alias: 'ex',
-            _description: 'execute the given remote cmd',
-            _events: {},
-            _eventsCount: 2 } ],
-        options:
-          [ { flags: '-V, --version',
-            required: 0,
-            optional: 0,
-            bool: true,
-            short: '-V',
-            long: '--version',
-            description: 'output the version number' },
-            { flags: '-C, --chdir <path>',
-              required: -13,
-              optional: 0,
-              bool: true,
-              short: '-C',
-              long: '--chdir',
-              description: 'change the working directory' },
-            { flags: '-c, --config <path>',
-              required: -14,
-              optional: 0,
-              bool: true,
-              short: '-c',
-              long: '--config',
-              description: 'set config path. defaults to ./deploy.conf' },
-            { flags: '-T, --no-tests',
-              required: 0,
-              optional: 0,
-              bool: false,
-              short: '-T',
-              long: '--no-tests',
-              description: 'ignore test hook' } ],
-        _execs: {},
-        _allowUnknownOption: false,
-        _args: new Array<string>(),
-        _name: 'gulp',
-        _version: '0.0.1',
-        _events: { version: new Array<string>() },
-        _eventsCount: 7,
-        tests: true,
-        rawArgs:
-          [ '/Users/1ambda/.nvm/versions/node/v5.2.0/bin/node',
-            '/Users/1ambda/.nvm/versions/node/v5.2.0/bin/gulp',
-            'con-test' ],
-        args: [ 'con-test' ]
-      };
-
+      const raw = SampleResources.command1;
       let c1: Command = Command.deserialize(Command, raw);
       expect(c1.commands.length).toEqual(2);
       expect(c1.commands[0].name).toEqual(raw.commands[0]._name);
