42805378efe4ef14adfd71dd601df160b8a3d8ed
1ambda <1amb4a@gmail.com>
2016-01-12 08:24:49 +0900

    feat: Change repo summary button color

--- viewer/app/github_user.jsx
+++ viewer/app/github_user.jsx
@@ -31,8 +31,54 @@ export default class GithubUser extends React.Component {
         display: "block",
         backgroundPositionY: "25%",
         margin: "0 auto"
+      },
+
+      btn_repo_summary_text: {
+        minWidth: "110px",
+        borderTopLeftRadius: "3px",
+        borderBottomLeftRadius: "3ox",
+        color: "white",
+        fontWeight: 400,
+        textShadow: "none"
+      },
+
+      btn_repo_summary_count: {
+        minWidth: "60px",
+        borderTopRightRadius: "3px",
+        borderBottomRightRadius: "3px"
       }
     };
+
+    let watchedButtonColor = "rgb(255, 107, 107)";
+    let starredButtonColor = "rgb(122, 98, 201)";
+    let forkedButtonColor = "rgb(11, 98, 164)";
+
+    this.style.btn_watched_text = Object.assign({
+      borderColor: watchedButtonColor,
+      backgroundColor: watchedButtonColor
+    }, this.style.btn_repo_summary_text);
+
+    this.style.btn_watched_count = Object.assign({
+      borderColor: watchedButtonColor
+    }, this.style.btn_repo_summary_count);
+
+    this.style.btn_starred_text = Object.assign({
+      borderColor: starredButtonColor,
+      backgroundColor: starredButtonColor
+    }, this.style.btn_repo_summary_text);
+
+    this.style.btn_starred_count = Object.assign({
+      borderColor: starredButtonColor
+    }, this.style.btn_repo_summary_count);
+
+    this.style.btn_forked_text = Object.assign({
+      borderColor: forkedButtonColor,
+      backgroundColor: forkedButtonColor
+    }, this.style.btn_repo_summary_text);
+
+    this.style.btn_forked_count = Object.assign({
+      borderColor: forkedButtonColor
+    }, this.style.btn_repo_summary_count);
 
     ProfileUtil.getUserPromise()
       .then(user => {
@@ -71,22 +117,26 @@ export default class GithubUser extends React.Component {
         <div className="row row-centered form-inline">
           <div className="input-group row-centered" style={{margin: "10px"}}>
             <div className="input-group-btn">
-              <button className="btn btn-danger" href="#" style={{minWidth: "110px"}}> <i className="fa fa-eye fa-lg"></i> Watched</button>
-              <button className="btn btn-default" style={{minWidth: "60px"}}>{this.state.repositorySummary.watchersCount}</button>
+              <button className="btn btn-default" href="#" style={this.style.btn_watched_text}>
+                <i className="fa fa-eye fa-lg"></i> Watched
+              </button>
+              <button className="btn btn-default" style={this.style.btn_watched_count}>
+                {this.state.repositorySummary.watchersCount}
+              </button>
             </div>
           </div>
 
           <di className="input-group row-centered" style={{margin: "10px"}}>
             <div className="input-group-btn">
-              <button className="btn btn-warning" href="#" style={{minWidth: "110px"}}> <i className="fa fa-star fa-lg"></i> Starred</button>
-              <button className="btn btn-default" style={{minWidth: "60px"}}>{this.state.repositorySummary.stargazersCount}</button>
+              <button className="btn btn-default" href="#" style={this.style.btn_starred_text}> <i className="fa fa-star fa-lg"></i> Starred</button>
+              <button className="btn btn-default" style={this.style.btn_starred_count}>{this.state.repositorySummary.stargazersCount}</button>
             </div>
           </di>
 
           <div className="input-group row-centered" style={{margin: "10px"}}>
             <div className="input-group-btn">
-              <button className="btn btn-info" href="#" style={{minWidth: "110px"}}> <i className="fa fa-code-fork fa-lg"></i> Forked</button>
-              <button className="btn btn-default" style={{minWidth: "60px"}}>{this.state.repositorySummary.forksCount}</button>
+              <button className="btn btn-info" href="#" style={this.style.btn_forked_text}> <i className="fa fa-code-fork fa-lg"></i> Forked</button>
+              <button className="btn btn-default" style={this.style.btn_forked_count}>{this.state.repositorySummary.forksCount}</button>
             </div>
           </div>
         </div>
