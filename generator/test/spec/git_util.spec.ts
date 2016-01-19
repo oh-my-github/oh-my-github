40304cd74da8c1d6e316d4082a09e30eaf67b7aa
1ambda <1amb4a@gmail.com>
2016-01-19 20:24:50 +0900

    chore: Add .spec to test files #10

--- generator/test/spec/git_util.spec.ts
+++ generator/test/spec/git_util.spec.ts
@@ -1,7 +1,7 @@
 /// <reference path="../../../typings/jasmine/jasmine.d.ts" />
 /// <reference path="../../../typings/node/node.d.ts" />
 
-describe("git_util.ts", () => {
+describe("git_util.spec.ts", () => {
   describe("", () => {
 
   });
